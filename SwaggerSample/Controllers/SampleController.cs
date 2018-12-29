45fba7a04994234b8b26cfa6840d8dd2bc499a28
VitoChiang <VitoChiang@outlook.com>
2018-12-29 18:43:33 +0800

    Add Sample API.

---
+++ SwaggerSample/Controllers/SampleController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace SwaggerSample.Controllers
+{
+    /// <summary>
+    /// 這是要 Demo Swagger 的 Sample Controller
+    /// </summary>
+    public class SampleController : ApiController
+    {
+        /// <summary>
+        /// 取得 Sample 文字
+        /// </summary>
+        /// <returns></returns>
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "Swagger", "Sample" };
+        }
+    }
+}
